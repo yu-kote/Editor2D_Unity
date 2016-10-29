6ae049cd8462102b5bd05bc0b3ebddcba1d3ad56
FUYUUNO1-PC\fuyuu.No1 <yu1229kotetu@gmail.com>
2016-10-29 13:03:08 +0900

    2Dエディター上げておく

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// ブロックを配置できるクラス
+/// </summary>
+public class BlockController : MonoBehaviour
+{
+
+
+    public int chip_num_x;
+    public int chip_num_y;
+
+    public float chip_size;
+
+    public GameObject[,] blocks;
+
+
+
+    void Start()
+    {
+        chip_num_x = 10;
+        chip_num_y = 10;
+        chip_size = 1.0f;
+
+        blocks = new GameObject[chip_num_y, chip_num_x];
+
+
+        for (int y = 0; y < chip_num_y; y++)
+        {
+            for (int x = 0; x < chip_num_x; x++)
+            {
+                GameObject block;
+                block = Resources.Load<GameObject>("Prefabs/BlockBase");
+
+                block.transform.position = new Vector2(chip_size * x, chip_size * y * -1);
+                block.transform.localScale = new Vector2(chip_size, chip_size);
+
+                blocks[y, x] = Instantiate(block);
+                //blocks[y, x].transform.parent = gameObject.transform;
+                //block.transform.parent = gameObject.transform;
+            }
+        }
+
+    }
+
+    GameObject getClickObj()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var colition = Physics2D.OverlapPoint(pos);
+
+            if (colition)
+            {
+                var hitobj = Physics2D.Raycast(pos, -Vector2.up);
+                if (hitobj)
+                {
+                    Debug.Log(hitobj.collider.gameObject.name);
+                    return hitobj.collider.gameObject;
+                }
+            }
+        }
+        return null;
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// グリッド線を引くクラス
+/// </summary>
+public class Grid : MonoBehaviour
+{
+    int grid_num = 100;
+    Vector2 grid_startpos;
+    Vector2[] grid_start = null;
+    Vector2[] grid_end = null;
+
+    float grid_length = 100;
+    float grid_interval = 1;
+
+    void Start()
+    {
+        grid_startpos = new Vector2(-50, 50);
+        grid_start = new Vector2[grid_num];
+        grid_end = new Vector2[grid_num];
+
+        LineRenderer renderer = gameObject.GetComponent<LineRenderer>();
+
+        //transform.parent = grids.transform;
+        for (int x = 0; x < grid_num; x++)
+        {
+            GameObject grids = new GameObject();
+            grids.AddComponent<LineRenderer>();
+
+            var lr = grids.GetComponent<LineRenderer>();
+            grids.transform.parent = gameObject.transform;
+
+            lr.SetWidth(0.05f, 0.05f);
+            lr.SetVertexCount(2);
+
+            grid_start[x].x = grid_length + grid_startpos.x;
+            grid_start[x].y = grid_interval * -x + grid_startpos.y;
+            grid_end[x].x = grid_length * -1 + grid_startpos.x;
+            grid_end[x].y = grid_interval * -x + grid_startpos.y;
+
+            lr.SetPosition(0, new Vector2(grid_start[x].x, grid_start[x].y));
+            lr.SetPosition(1, new Vector2(grid_end[x].x, grid_end[x].y));
+        }
+        for (int y = 0; y < grid_num; y++)
+        {
+            GameObject grids = new GameObject();
+            grids.AddComponent<LineRenderer>();
+
+            var lr = grids.GetComponent<LineRenderer>();
+            grids.transform.parent = gameObject.transform;
+
+            lr.SetWidth(0.05f, 0.05f);
+            lr.SetVertexCount(2);
+
+            grid_start[y].x = grid_interval * y + grid_startpos.x;
+            grid_start[y].y = grid_length + grid_startpos.y;
+            grid_end[y].x = grid_interval * y + grid_startpos.x;
+            grid_end[y].y = grid_length * -1 + grid_startpos.y;
+
+            lr.SetPosition(0, new Vector2(grid_start[y].x, grid_start[y].y));
+            lr.SetPosition(1, new Vector2(grid_end[y].x, grid_end[y].y));
+        }
+
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
