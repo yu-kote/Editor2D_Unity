0c426dc1a639f4a0f872ad7517e915f49a7abdc5
FUYUUNO1-PC\fuyuu.No1 <yu1229kotetu@gmail.com>
2016-12-05 12:06:14 +0900

    ファイル階層うつし

---
+++ Assets/Scripts/EditScene/EditSelectScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EditSelectScene : MonoBehaviour
+{
+    public static EditSelectScene instance = new EditSelectScene();
+
+    public string select_scene = null;
+
+
+    public void selectSchool()
+    {
+        instance.select_scene = "School";
+        SceneManager.LoadScene("Editor");
+    }
+
+    public void selectHospital()
+    {
+        instance.select_scene = "Hospital";
+        SceneManager.LoadScene("Editor");
+    }
+
+}
